6ad1bb7107dfd5adc8da65391c6af938780d9f59
Faheem Saleem <63436748+PinchedRainbow@users.noreply.github.com>
2022-07-11 10:54:36 +0100

    Update 1.0 for main program.cs - Read Description
    *Fixed merge movement bugs
    *Added save game, load game
    *Scoring system
    *Move randomiser (will add time delay soon)
    *Added board size configuration
    *Updated board layout with spacings
    *Rewrote code with more appropriate naming's and method's

--- _2048/Board.cs
+++ _2048/Board.cs
@@ -1,315 +1,250 @@
 using System;
-using System.Linq;
+using System.IO;
 
 namespace _2048
 {
-    public class Board
+    internal class Program
     {
-        private readonly Tile[,] board;
-        private readonly int size;
-
-        public Board(int size)
+        // initialising variables
+        private static bool _quit = false;
+        private static readonly string FilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments), @"2048_Console\");
+        private static bool AI = false;
+        
+        private static void Main(string[] args)
         {
-            this.size = size;
-            board = new Tile[size, size];
-
-            for (int i = 0; i < size; i++)
+            // check if directory for saving game exists
+            if (!Directory.Exists(FilePath))
             {
-                for (int j = 0; j < size; j++)
-                {
-                    board[i, j] = new Tile(0, "", i, j);
-                }
+                Directory.CreateDirectory(FilePath);
+            }
+            
+            // ask if user wants to play against AI
+            Console.WriteLine("Do you want the moves to be random {Faheem's AI}? (y/n)");
+            string aiAns = Console.ReadLine();
+            if (aiAns == "y")
+            {
+                AI = true;
             }
 
-            //genInitTiles();
+            // ask user if they want to load game or start new one
+            Console.WriteLine("Do you want to load game or start new one? (L/N)");
+            string answer = Console.ReadLine();
+            if (answer.ToLower() == "l")
+            {
+                try
+                {
+                    // ask user for file name
+                    Console.WriteLine("Enter file name:");
+                    string fileName = Console.ReadLine();
+                    LoadGame(fileName);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+            else
+            {
+                PlayGame(new Board(GetBoardSize(),0), false, AI);
+            }
         }
 
-
-        public void PrintBoard()
+        private static void LoadGame(string fileName)
         {
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                {
-                    if (board[i, j].Number.ToString().Length == 5) Console.Write(board[i, j].Number + " ");
-                    else if (board[i, j].Number.ToString().Length == 4) Console.Write(board[i, j].Number + "  ");
-                    else if (board[i, j].Number.ToString().Length == 3) Console.Write(board[i, j].Number + "   ");
-                    else if (board[i, j].Number.ToString().Length == 2) Console.Write(board[i, j].Number + "    ");
-                    else Console.Write(board[i, j].Number + "     ");
-                }
-                Console.WriteLine();
-                Console.WriteLine();
-            }
+            // get file path
+            string gamePath = FilePath + fileName + ".txt";
+            // read file from path
+            string game = File.ReadAllText(gamePath);
+            // read first line of file to get board size
+            int boardSize = int.Parse(game.Split('\n')[0]);
+            // read second line of file to get score
+            int score = int.Parse(game.Split('\n')[1]);
+            var b = new Board(boardSize,score);
+            // parse file to board
+            b.ReadBoard(game);
+            PlayGame(b, true, AI);
         }
         
-        public void genRandomTile()
+        private static void PlayGame(Board b, bool loadedGame, bool AI)
         {
-            int x, y;
-            var rnd = new Random();
             while (true)
             {
-                x = rnd.Next(0, size);
-                y = rnd.Next(0, size);
-                if (board[x, y].Number != 0) continue;
-                var value = rnd.Next(0,10) < 9 ? 2 : 4;
-                board[x, y].Number = value;
-                break;
-            }
-        }
-
-        public bool isGameOver()
-        {
-            bool ans = true;
-
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
+                // checks if the game was loaded from file or not
+                // if the game was loaded from file, the board is already initialized and it doesnt need to have a random tile added to it
+                if (loadedGame) loadedGame = false;
+                else b.GenRandomTile();
+                
+                b.PrintBoard();
+                Console.WriteLine("Your current score is {0}", b.Score);
+                Console.WriteLine("(L)eft, (R)ight, (U)p, (D)own, (S)ave or (Q)uit");
+                
+                if (AI)
                 {
-                    if (board[i, j].Number == 0)
+                    // make random move for AI
+                    // choose random direction
+                    var rand = new Random();
+                    int randDir = rand.Next(0, 4);
+                    // make move
+                    switch (randDir)
                     {
-                        ans = false;
-                    }
-                }
-            }
-
-            for (int i = 0; i < size - 1; i++)
-            {
-                for (int j = 0; j < size - 1; j++)
-                {
-                    if (board[i, j].Number == board[i+1,j].Number)
-                    {
-                        ans = false;
-                    }
-                    else if (board[i, j].Number == board[i, j + 1].Number)
-                    {
-                        ans = false;
-                    }
-                }
-            }
-
-            for (int i = 0; i < size - 1; i++)
-            {
-                if (board[size-1, i].Number == board[size-1, i + 1].Number)
-                {
-                    ans = false;
-                }
-                else if (board[i,size-1].Number == board[i+1,size-1].Number)
-                {
-                    ans = false;
-                }
-            }
-
-            return ans;
-        }
-
-        public void moveLeft()
-        {
-            //merge the tiles
-            mergeTilesOnLeft();
-            //please move the tiles to the left
-            for (int i = 0; i < size; i++)
-            for (int j = 0; j < size; j++)
-                if (board[i, j].Number == 0)
-                    for (int k = j + 1; k < size; k++)
-                        if (board[i, k].Number != 0)
-                        {
-                            board[i, j].Number = board[i, k].Number;
-                            board[i, k].Number = 0;
+                        case 0:
+                            // left
+                            LeftFunction(b);
                             break;
-                        }
-        }
-
-        public void moveRight()
-        {
-            mergeTilesOnRight();
-            for (int i = 0; i < size - 1; i++)
-            {
-                for (int j = size - 1; j > 0; j--)
-                {
-                    if (board[i, j].Number == 0)
-                    {
-                        for (int k = j - 1; k >= 0; k--)
-                        {
-                            if (board[i, k].Number != 0)
-                            {
-                                board[i, j].Number = board[i, k].Number;
-                                board[i, k].Number = 0;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-            
-            
-        }
-
-        public void moveUp()
-        {
-            mergeTilesOnUp();
-            //please move the tiles to the up
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                {
-                    if (board[j, i].Number != 0)
-                    {
-                        continue;
-                    }
-
-                    for (int k = j + 1; k < size; k++)
-                    {
-                        if (board[k, i].Number == 0)
-                        {
-                            continue;
-                        }
-                        board[j, i].Number = board[k, i].Number;
-                        board[k, i].Number = 0;
-                        break;
-                    }
-                }
-            }
-            
-            
-            
-        }
-
-        public void moveDown()
-        {
-            mergeTilesOnDown();
-            // PLEASE MOVE TILES TO THE DOWN
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = size - 1; j > 0; j--)
-                {
-                    if (board[j, i].Number == 0)
-                    {
-                        for (int k = j - 1; k >= 0; k--)
-                        {
-                            if (board[k, i].Number != 0)
-                            {
-                                board[j, i].Number = board[k, i].Number;
-                                board[k, i].Number = 0;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-            
-        }
-
-        public void mergeTilesOnDown()
-        {
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = size-1; j >= 0; j--)
-                {
-                    if (board[j, i].Number == 0)
-                    {
-                        continue;
-                    }
-                    for (int k = j - 1; k >= 0; k--)
-                    {
-                        if (board[k, i].Number == 0)
-                        {
-                            continue;
-                        }
-                        if (board[j, i].Number == board[k, i].Number)
-                        {
-                            board[j, i].Number += board[k, i].Number;
-                            board[k, i].Number = 0;
+                        case 1:
+                            // right
+                            RightFunction(b);
                             break;
-                        }
+                        case 2:
+                            // up
+                            UpFunction(b);
+                            break;
+                        case 3:
+                            // down
+                            DownFunction(b);
+                            break;
                     }
                 }
+                else
+                {
+                    // human is playing, get input from user
+                    var ch = Console.ReadKey(false).Key;
+                    switch (ch)
+                    {
+                        case ConsoleKey.RightArrow:
+                            RightFunction(b);
+                            continue;
+
+                        case ConsoleKey.LeftArrow:
+                            LeftFunction(b);
+                            continue;
+
+                        case ConsoleKey.UpArrow:
+                            UpFunction(b);
+                            continue;
+
+                        case ConsoleKey.DownArrow:
+                            DownFunction(b);
+                            continue;
+                    
+                        case ConsoleKey.S:
+                            // save game
+                            SaveGame(b);
+                            continue;
+                    
+                        case ConsoleKey.Q:
+                            // quits game
+                            Console.WriteLine("\nQuitting game...");       
+                            _quit = true;
+                            break;
+                    }
+                    
+                }
+                
+                if (_quit) break;
             }
         }
 
-        public void mergeTilesOnRight()
-        {
-            // merge tiles that have been moved to the right, check if adjacant tiles are the same, if so, merge them and add the value of the two tiles to the first tile all the way to the right
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = size-1; j >=0; j--)
-                {
-                    if (board[i, j].Number == 0)
-                    {
-                        continue;
-                    }
 
-                    for (int k = j - 1; k >= 0; k--)
-                    {
-                        if (board[i, k].Number == 0)
-                        {
-                            continue;
-                        }
-                        if (board[i, j].Number == board[i, k].Number)
-                        {
-                            board[i, j].Number *= 2;
-                            board[i, k].Number = 0;
-                            break;
-                        }
-                    }
+        private static void LeftFunction(Board b)
+        {
+            Console.WriteLine("Left move made");
+            CheckForGameOver(b);
+            b.moveLeft();
+            b.addScore(b.mergeTilesOnLeft());
+            b.moveLeft();
+        }
+        
+        private static void UpFunction(Board b)
+        {
+            Console.WriteLine("Up move made");
+            CheckForGameOver(b);
+            b.moveUp();
+            b.addScore(b.mergeTilesOnUp());
+            b.moveUp();
+        }
+
+        private static void DownFunction(Board b)
+        {
+            Console.WriteLine("Down move made");
+            CheckForGameOver(b);
+            b.moveDown();
+            b.addScore(b.mergeTilesOnDown());
+            b.moveDown();
+        }
+
+        private static void RightFunction(Board b)
+        {
+            Console.WriteLine("Right move made");
+            CheckForGameOver(b);
+            b.moveRight();
+            b.addScore(b.mergeTilesOnRight());
+            b.moveRight();
+        }
+
+
+        private static void CheckForGameOver(Board b)
+        {
+            if (!b.IsGameOver()) return;
+            Console.WriteLine("Game over");
+            _quit = true;
+        }
+        
+        private static int GetBoardSize()
+        {
+            int size;
+            Console.WriteLine("Enter a board size (leave empty for default of 4) : ");
+            string boardSize = Console.ReadLine();
+            if (boardSize == "")
+            {
+                size = 4;
+            }
+            else
+            {
+                // ensure that board size is between 2 and 20
+                size = int.Parse(boardSize);
+                while (size < 2 || size > 20)
+                {
+                    Console.WriteLine("Invalid board size. Enter a board size: ");
+                    size = int.Parse(Console.ReadLine()!);
                 }
             }
 
+            return size;
         }
 
-        public void mergeTilesOnUp()
+        private static void SaveGame(Board board)
         {
-            // merge tiles that have been moved towards up, check if adjacant tiles are the same, if so, merge them and add the value of the two tiles to the first tile all the way to the top
-            for (int i = 0; i < size; i++)
+            Console.WriteLine("\nEnter file name: ");
+            string fileName = Console.ReadLine() + ".txt";
+            string currentGame = board.ToString();
+            string fullfilePath = FilePath + fileName;
+                        
+            // check if file exists
+            if (File.Exists(fullfilePath))
             {
-                for (int j = 0; j < size; j++)
+                Console.WriteLine("File already exists. Do you want to overwrite it? (y/n)");
+                string answer = Console.ReadLine();
+                if (answer == "y")
                 {
-                    if (board[j, i].Number == 0)
-                    {
-                        continue;
-                    }
-                    for (int k = j + 1; k < size; k++)
-                    {
-                        if (board[k, i].Number == 0)
-                        {
-                            continue;
-                        }
-                        if (board[j, i].Number == board[k, i].Number)
-                        {
-                            board[j, i].Number += board[k, i].Number;
-                            board[k, i].Number = 0;
-                            break;
-                        }
-                    }
+                    File.WriteAllText(fullfilePath, currentGame);
+                    Console.WriteLine("\nGame saved to " + FilePath + fileName);
+                    Console.WriteLine("Enter any key to continue...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Game not saved!");
+                    Console.WriteLine("Enter any key to continue...");
+                    Console.ReadKey();
                 }
             }
-        }
-
-        public void mergeTilesOnLeft()
-        {
-            // merge tiles that have been moved towards the left, check if adjacent tiles are the same, if so, merge them and make one tile with the sum of the two tiles all the way to the left
-            for (int i = 0; i < size; i++)
+            else
             {
-                for (int j = 0; j < size; j++)
-                {
-                    if (board[i, j].Number == 0)
-                    {
-                        continue;
-                    }
-
-                    for (int k = j + 1; k < size; k++)
-                    {
-                        if (board[i, k].Number == 0)
-                        {
-                            continue;
-                        }
-                        if (board[i, j].Number == board[i, k].Number)
-                        {
-                            board[i, j].Number *= 2;
-                            board[i, k].Number = 0;
-                            break;
-                        }
-                    }
-                }
+                File.WriteAllText(fullfilePath, currentGame);
+                Console.WriteLine("\nGame saved to " + FilePath + fileName);
+                Console.WriteLine("Enter any key to continue...");
+                Console.ReadKey();
             }
         }
     }
